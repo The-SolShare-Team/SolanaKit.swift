8beef39762a9986cc1f98a5064594b3c67bdc320
Samuel Martineau <samuel@smartineau.me>
2025-10-14 17:13:05 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "SolanaKit",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "SolanaKit", targets: ["SolanaKit"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "SolanaKit",
+         url: "https://github.com/The-SolShare-Team/SolanaKit/releases/download/v0.0.1/SolanaKit.xcframework.zip",
+         checksum: "4a2f7ad0d36cbc510fdfb0ad173ad4dc0b8db2edd7ddbc5a6577265ef13567a6")
+   ]
+)
